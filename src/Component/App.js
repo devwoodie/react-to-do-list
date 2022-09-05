e3894478d7533363e6680eb31f7f14fc152d0550
devwoodie <ehddl453@naver.com>
2022-09-05 23:48:38 +0900

    feat : new project setting

---
+++ src/Component/App.js
@@ -0,0 +1,14 @@
+import '../Css/App.css';
+import '../Css/Reset.css';
+import Main from "./Main";
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="logo">MY LIST</h1>
+            <Main />
+        </div>
+    );
+}
+
+export default App;
