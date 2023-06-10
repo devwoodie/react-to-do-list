755c7fa5f90da57f2a574c9e29af9afc6a54a52b
devwoodie <ehddl453@naver.com>
2023-06-10 21:35:33 +0900

    feat : 쿠키 테스트2

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,25 +1,12 @@
 import '../Css/App.css';
 import '../Css/Reset.css';
 import Main from "./Main";
-import {useEffect, useState} from "react";
 
 const App = () => {
-
-    const [token, setToken] = useState("토큰입니다.");
-
-    const handleCookie = () => {
-        try{
-            document.cookie = `token=${token}; path=/`;
-        }catch (err){
-            console.log(err);
-        }
-    }
-
     return (
         <div className="App">
             <div className="container">
                 <h1 className="logo">MY LIST</h1>
-                <button className="test-cookie" onClick={handleCookie}>쿠키 테스트</button>
                 <Main />
             </div>
         </div>
