dd0e6d4f9b3de5f3fda89f54d15c551205ab7200
devwoodie <ehddl453@naver.com>
2023-06-10 21:38:02 +0900

    feat : 쿠키, 토큰 테스트

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,12 +1,23 @@
 import '../Css/App.css';
 import '../Css/Reset.css';
 import Main from "./Main";
+import {useState} from "react";
 
 const App = () => {
+
+    const [token, setToken] = useState("")
+
+    const handleCookie = () => {
+        const receivedToken = 'qs토wd큰ef입rg니th다oj';
+        document.cookie = `token=${receivedToken}; path=/`;
+        setToken(receivedToken);
+    }
+
     return (
         <div className="App">
             <div className="container">
                 <h1 className="logo">MY LIST</h1>
+                <button onClick={handleCookie}>토큰 테스트 버튼</button>
                 <Main />
             </div>
         </div>
