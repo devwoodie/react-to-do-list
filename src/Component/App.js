317f59a78901e50dfc39c00604d33f41c2ff8dec
devwoodie <ehddl453@naver.com>
2023-06-11 10:56:46 +0900

    feat : 파라미터 토큰 테스트1

--- src/Component/App.js
+++ src/Component/App.js
@@ -2,11 +2,13 @@ import '../Css/App.css';
 import '../Css/Reset.css';
 import Main from "./Main";
 import {useState} from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
 
     const [token, setToken] = useState("qs토wd큰ef입rg니th다oj")
 
+    // 쿠키로 토큰 전송
     const handleCookie = () => {
         document.cookie = `token=${token}; path=/`;
         const cookies = get_cookie("token");
@@ -19,11 +21,19 @@ const App = () => {
         return value? value[2] : null;
     }
 
+    // 파라미터로 토큰 전송
+    const handleButtonClick = () => {
+        // 토큰을 파라미터로 사용하여 웹 페이지로 이동하는 로직을 추가합니다.
+        const url = `?token=${encodeURIComponent(token)}`;
+        window.location.href = url;
+    };
+
     return (
         <div className="App">
             <div className="container">
                 <h1 className="logo">MY LIST</h1>
                 <button onClick={handleCookie}>토큰 테스트 버튼</button>
+                <button onClick={handleButtonClick}>파라미터 테스트</button>
                 <Main />
             </div>
         </div>
